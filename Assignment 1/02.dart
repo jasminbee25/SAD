c0aaf2c6939e17c84a2054fd71699d255f755ce0
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-11 19:59:17 +0600

    Update 02.dart

--- Assignment 1/02.dart
+++ Assignment 1/02.dart
@@ -1,1 +1,9 @@
-
+void main(){
+    //single quotes
+    print('Hello I am "John Doe"');
+    print('Hello I\'am "John Doe"');
+    
+    //double quotes
+    print("Hello I am \"John Doe\"");
+    print("Hello I'am \"John Doe\"");
+}
