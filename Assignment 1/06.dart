fd043e00416ecd805263ede5c3209e1f829917c6
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-11 21:15:31 +0600

    Update 06.dart

--- Assignment 1/06.dart
+++ Assignment 1/06.dart
@@ -1,6 +1,10 @@
 import 'dart:io';
-
 void main(){
-    int x = int.parse(stdin.readLineSync()!);
-    print(x * x * x);
+    stdout.write('First name: ');
+    String first = stdin.readLineSync()!;
+    
+    stdout.write('Last name: ');
+    String last = stdin.readLineSync()!;
+    print('Full name: $first $last');
+
 }
