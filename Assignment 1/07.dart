87a0570fd7f0420d33adce3a3849a3bba5cf417e
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-11 21:21:29 +0600

    Update 07.dart

--- Assignment 1/07.dart
+++ Assignment 1/07.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
 void main(){
-     int length = 5;
-     int breadth = 3;
-     print("Area = ${length * breadth}");
+    stdout.write('Enter first number: ');
+    int a = int.parse(stdin.readLineSync()!);
+    
+    stdout.write('Enter last number: ');
+    int b = int.parse(stdin.readLineSync()!);
+    
+    int res = a ~/ b;
+    int rem = a % b;
+    
+    print("Quotient : $res \nReminder : $rem");
 }
