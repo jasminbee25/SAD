3960b68297eee0bb38b492279030ae6cd6e41888
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-11 21:55:22 +0600

    Update 12.dart

--- Assignment 1/12.dart
+++ Assignment 1/12.dart
@@ -1,1 +1,11 @@
-
+import 'dart:io';
+void main(){
+    int dis = 25;
+    int speed = 40;
+    
+    double timeH = dis / speed;
+    double timeM = timeH * 60;
+    
+    print("Time to reach : $timeM minutes");
+    
+}    
