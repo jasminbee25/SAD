066ef02e026ad374712c0f8d66e867385e7ad23a
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 20:28:01 +0600

    Update 04.dart

--- Assignment 2/Practice 3/04.dart
+++ Assignment 2/Practice 3/04.dart
@@ -1,1 +1,14 @@
-
+import 'dart:math';
+void main(){
+    print(generatePassword(8));//password length = 8
+}
+String generatePassword(int length){
+    const chars = 'ahsvehgdbehbedhebfrhfgueAHVSGAJ143345@#\$%&';
+    Random random = Random();
+    String password = '';
+    for(int i=0; i<length; i++){
+        password += chars[random.nextInt(chars.length)];
+        
+    }
+    return password;
+}
