4081196b4859b87f5902525ddd751623bce733e5
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 20:18:58 +0600

    Update 02.dart

--- Assignment 2/Practice 3/02.dart
+++ Assignment 2/Practice 3/02.dart
@@ -1,1 +1,12 @@
+import 'dart:io';
+void main(){
+    printEvenNumbers(1,20);
+}
 
+void printEvenNumbers(int start, int end){
+    for(int i=start; i<=end; i++){
+        if(i % 2 == 0){
+            print(i);
+        }
+    }
+}
