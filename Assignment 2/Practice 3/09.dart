94876f50b7c963f49668409b5cb5ed36dee81785
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 20:53:49 +0600

    Update 09.dart

--- Assignment 2/Practice 3/09.dart
+++ Assignment 2/Practice 3/09.dart
@@ -1,1 +1,10 @@
-
+import 'dart:math';
+void main(){
+    print(maxNumber(10, 25,15));
+}
+int maxNumber (int a, int b, int c){
+    if(a >= b && a>= c) return a;
+    else if(b >= a && b >= c) return b;
+    else return c;
+    
+}
