76de731a14381402561781789b84ba45b8a8741c
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 19:37:42 +0600

    Update 02.dart

--- Assignment 2/Practice 2/02.dart
+++ Assignment 2/Practice 2/02.dart
@@ -1,1 +1,10 @@
-
+import 'dart:io';
+void main(){
+    stdout.write('Enter a number: ');
+    String ch = stdin.readLineSync()!.toLowerCase();
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+        print('$ch is a vowel');
+    }else{
+        print('$ch is a consonant');
+    }
+}
