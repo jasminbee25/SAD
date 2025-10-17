753a04c6e159b78b3c99e05d4b06774e5acf6291
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 19:41:32 +0600

    Update 03.dart

--- Assignment 2/Practice 2/03.dart
+++ Assignment 2/Practice 2/03.dart
@@ -1,1 +1,12 @@
-
+import 'dart:io';
+void main(){
+    stdout.write('Enter a number: ');
+    double n = double.parse(stdin.readLineSync()!);
+    if (n > 0){
+        print('$n is positive');
+    }else if (n < 0){
+        print('$n is negitive');
+    }else{
+        print('Number is zero');
+    }
+}
