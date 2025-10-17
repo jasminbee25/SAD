30b6968bf27760e30ed9064e7f7960d9ad1dc0e7
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 20:05:48 +0600

    Update 08.dart

--- Assignment 2/Practice 2/08.dart
+++ Assignment 2/Practice 2/08.dart
@@ -1,1 +1,25 @@
-
+import 'dart:io';
+void main(){
+    stdout.write('Enter a number: ');
+    double a = double.parse(stdin.readLineSync()!);
+    stdout.write('Enter operator (+,-,*,/): ');
+    String op = stdin.readLineSync()!;
+    stdout.write('Enter second number: ');
+    double b = double.parse(stdin.readLineSync()!);
+    
+    double result;
+    if(op == '+'){
+        result = a + b;
+    }else if(op == '-'){
+        result = a - b;
+    }else if(op == ' * '){
+        result = a * b;
+    }else if(op == ' / '){
+        result = a / b;
+    }else{
+        print('Invalid operator');
+        return;
+    }
+    print('Result = $result');
+    
+}
