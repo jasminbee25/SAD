fc7c9bb767266af560d42aa7e2fefb9f01981338
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 19:31:06 +0600

    Update 01.dart

--- Assignment 2/Practice 2/01.dart
+++ Assignment 2/Practice 2/01.dart
@@ -1,1 +1,11 @@
-
+import 'dart:io';
+void main(){
+    stdout.write('Enter a number: ');
+    int n = int.parse(stdin.readLineSync()!);
+    if(n % 2 == 0){
+        
+        print('$n is even');
+    }else{
+        print('$n is odd');
+    }    
+}
