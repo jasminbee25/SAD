affa7c7332db609a1e1bf56ccf053f8a0a39cbfa
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 21:04:51 +0600

    Update 11.dart

--- Assignment 2/Practice 3/11.dart
+++ Assignment 2/Practice 3/11.dart
@@ -1,1 +1,8 @@
-
+import 'dart:io';
+void main(){
+    createUser("Jasmin", 22);
+    createUser("Halima", 24, false);
+}
+void createUser(String name, int age, [bool isActive = true]){
+    print("Name: $name, Age: $age, Active: $isActive");
+}
