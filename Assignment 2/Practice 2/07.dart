14c2bda3a3a571b386e40c2262261b5cc204aaa3
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 19:55:30 +0600

    Update 07.dart

--- Assignment 2/Practice 2/07.dart
+++ Assignment 2/Practice 2/07.dart
@@ -1,1 +1,10 @@
-
+import 'dart:io';
+void main(){
+    for(int num=1;num<=9;num++){
+        print('Table of $num:');
+        for(int i=1; i<=10; i++){
+            print('$num * $i = ${num * i}');
+        }
+        print('');//empty line between tables
+    }
+}
