554aa1b074fa2c3200fd4f172e221750aee57092
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-17 19:47:50 +0600

    Update 05.dart

--- Assignment 2/Practice 2/05.dart
+++ Assignment 2/Practice 2/05.dart
@@ -1,1 +1,10 @@
-
+import 'dart:io';
+void main(){
+    stdout.write('Enter n: ');
+    int n = int.parse(stdin.readLineSync()!);
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+    print('Sum = $sum');
+}
