42049a76f2dceadf39f50b3a9c33d18015683b1e
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-20 14:55:45 +0600

    Update 07.dart

--- Assignment 3/Practice 5/07.dart
+++ Assignment 3/Practice 5/07.dart
@@ -1,1 +1,18 @@
+import 'dart:io';
 
+void main(){
+  File file = File('Practice 5/students.csv');
+
+  List<String> students = [
+    'Name,Age,Address',
+    'Jasmin,22,Sylhet',
+    'Hafsa,21,Dhaka',
+    'Halima,25,Barishal'
+  ];
+
+  file.writeAsStringSync(students.join('\n'));
+  print('Students data written to students.csv\n');
+
+  String content = file.readAsStringSync();
+  print('Reading students.csv:\n$content');
+}
