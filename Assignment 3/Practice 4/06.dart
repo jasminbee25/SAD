563e560f28ef115221a0c98ce9f618634ad71fd8
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-20 13:51:59 +0600

    Update 06.dart

--- Assignment 3/Practice 4/06.dart
+++ Assignment 3/Practice 4/06.dart
@@ -1,1 +1,16 @@
-
+import 'dart:io';
+void main() {
+    Map<String, dynamic> person = {
+        "name": "Jasmin",
+        "address": "Sunamganj",
+        "age": 22,
+        "country": "Bangladesh"
+    };
+    
+    person["country"] = "France"; // updating country
+    
+    print("Updated Map:");
+    person.forEach((key,value){
+        print("$key: $value");
+    });
+}
