6b0f605737698c8a65bde3d2e2e788927439bceb
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-20 13:56:21 +0600

    Update 07.dart

--- Assignment 3/Practice 4/07.dart
+++ Assignment 3/Practice 4/07.dart
@@ -1,1 +1,12 @@
-
+import 'dart:io';
+void main() {
+    Map<String, String> contacts = {
+        "John": "01713333333",
+        "Lina": "01711111111",
+        "Alex": "01722222222",
+        "Eva": "01744444444"
+    } ;
+    var keysWithLength4= contacts.keys.where((key) => key.length == 4);
+    print("Keys with length 4:");
+    print(keysWithLength4);
+}
