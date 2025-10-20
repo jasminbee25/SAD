1979ee1b1ecd29cb51f79511629c4da9073cd449
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-20 14:26:49 +0600

    Update 01.dart

--- Assignment 3/Practice 5/01.dart
+++ Assignment 3/Practice 5/01.dart
@@ -1,1 +1,11 @@
+import 'dart:io';
 
+void main() {
+  var file = File('hello.txt');
+  try {
+    file.writeAsStringSync('Jasmin');
+    print('Name written to hello.txt');
+  } catch (e) {
+    print('Error: $e');
+  }
+}
