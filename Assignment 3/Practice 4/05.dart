844b2e6481a7e4745633cf9c77bb1b83a84333d2
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-20 13:47:31 +0600

    Update 05.dart

--- Assignment 3/Practice 4/05.dart
+++ Assignment 3/Practice 4/05.dart
@@ -1,1 +1,7 @@
-
+import 'dart:io';
+void main() {
+    List<String> friends = ["Ayesha","Jasmin","Halima","Nila","Arbee","Anabhi"];
+    var namesStartingWithA = friends.where((name) => name.startsWith('A'));
+    print("Friends whose names start with 'A':");
+    print(namesStartingWithA);
+}
