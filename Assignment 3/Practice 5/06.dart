aab3eec340daff44703da81386a582a9b18402ae
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-20 14:54:27 +0600

    Update 06.dart

--- Assignment 3/Practice 5/06.dart
+++ Assignment 3/Practice 5/06.dart
@@ -1,1 +1,11 @@
+import 'dart:io';
 
+void main() {
+  File file = File('hello_copy.txt');
+  if (file.existsSync()) {
+    file.deleteSync();
+    print('hello_copy.txt deleted.');
+  } else {
+    print('File does not exist.');
+  }
+}
