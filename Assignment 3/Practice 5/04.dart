376ddaadc48766fb2a68df5065cc69aa18477f31
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-20 14:50:58 +0600

    Update 04.dart

--- Assignment 3/Practice 5/04.dart
+++ Assignment 3/Practice 5/04.dart
@@ -1,1 +1,9 @@
 
+import 'dart:io';
+
+void main(){
+  File src = File("Practice 5/hello.txt");
+  File des = File("Practice 5/hello_copy.txt");
+  src.copySync(des.path);
+  print("File copied to hello_copy.txt");
+}
