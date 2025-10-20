7f54c749ce9335651aa45018ec6a801fc4387f3f
jasminbee25 <jasminbagom1208@gmail.com>
2025-10-20 14:53:15 +0600

    Update 05.dart

--- Assignment 3/Practice 5/05.dart
+++ Assignment 3/Practice 5/05.dart
@@ -1,1 +1,9 @@
+import 'dart:io';
 
+void main() {
+  for (int i = 1; i <= 100; i++) {
+    File file = File('Practice 5/Created Files/file_$i.txt');
+    file.writeAsStringSync('This is file number $i');
+  }
+  print('100 files created successfully!');
+}
