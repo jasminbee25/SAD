7144ef41b53a2fec670f417220f7d07544ebad60
jasminbee25 <jasminbagom1208@gmail.com>
2025-11-03 22:25:22 +0600

    Update 01.dart

--- Assignment 4/Practice 6/01.dart
+++ Assignment 4/Practice 6/01.dart
@@ -1,1 +1,21 @@
+void main(){
+    var laptop1 = Laptop(1, "HP", "8GB");
+    var laptop2 = Laptop(2, "Dell", "16GB");
+    var laptop3 = Laptop(3, "Lenovo", "12GB");
 
+    laptop1.show();
+    laptop2.show();
+    laptop3.show();
+}
+
+class Laptop{
+    int id;
+    String name;
+    String ram;
+
+    Laptop(this.id, this.name, this.ram);
+
+    void show(){
+        print("ID: $id, Name: $name, RAM: $ram");
+    }
+}
