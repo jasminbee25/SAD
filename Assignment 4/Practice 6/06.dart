5740f2930be2ff6b0c3e6e328c01277be4a60ca0
jasminbee25 <jasminbagom1208@gmail.com>
2025-11-03 22:48:25 +0600

    Update 06.dart

--- Assignment 4/Practice 6/06.dart
+++ Assignment 4/Practice 6/06.dart
@@ -1,1 +1,19 @@
+abstract class Bottle{
+  void open();
 
+  factory Bottle.create(){
+    return CokeBottle();
+  }
+}
+
+class CokeBottle implements Bottle{
+  @override
+  void open(){
+    print("Coke bottle is opened");
+  }
+}
+
+void main(){
+  Bottle B = Bottle.create();
+  B.open();
+}
