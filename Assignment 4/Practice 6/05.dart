2e6b9f787fcb4846a3b33a24888ca85005ca85e9
jasminbee25 <jasminbagom1208@gmail.com>
2025-11-03 22:47:05 +0600

    Update 05.dart

--- Assignment 4/Practice 6/05.dart
+++ Assignment 4/Practice 6/05.dart
@@ -1,1 +1,29 @@
+class Camera {
+  int _I;
+  String _B;
+  String _C;
+  int _P;
 
+  Camera(this._I, this._B, this._C, this._P);
+
+  int get id => _I;
+  String get brand => _B;
+  String get color => _C;
+  int get price => _P;
+
+  set price(int val) => _P = val;
+
+  void PRINT() {
+    print("ID: $_I, Brand: $_B, Color: $_C, Price: $_P");
+  }
+}
+
+void main() {
+  Camera c1 = Camera(1, "Canon", "Black", 50000);
+  Camera c2 = Camera(2, "Nikon", "Gray", 65000);
+  Camera c3 = Camera(3, "Sony", "Silver", 80000);
+  
+  c1.PRINT();
+  c2.PRINT();
+  c3.PRINT();
+}
