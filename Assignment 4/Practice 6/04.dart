bfc499d460c875de965e140482658cc1d54b7218
jasminbee25 <jasminbagom1208@gmail.com>
2025-11-03 22:40:36 +0600

    Update 04.dart

--- Assignment 4/Practice 6/04.dart
+++ Assignment 4/Practice 6/04.dart
@@ -1,1 +1,19 @@
+void main(){
+    var cat = Cat(1, "Persian Cat", "White", "Meow");
+    cat.show();
+}
+class Animal{
+    int id;
+    String name;
+    String color;
 
+    Animal(this.id, this.name, this.color);
+}
+class Cat extends Animal {
+    String sound;
+    Cat(int id, String name, String color, this.sound) : super(id, name, color);
+
+    void show(){
+        print("ID: $id, Name: $name, Color: $color, Sound: $sound");
+    }
+}
