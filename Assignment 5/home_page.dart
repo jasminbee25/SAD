12a3494ffa4c0c889ab721752add94247634f731
jasminbee25 <jasminbagom1208@gmail.com>
2025-11-24 23:28:24 +0600

    Update home_page.dart

--- Assignment 5/home_page.dart
+++ Assignment 5/home_page.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("Home Page",
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.home, color: Colors.white)),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings, color: Colors.white),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome!",
+          style: TextStyle(fontSize: 24, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
