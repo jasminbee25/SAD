6d7a33855f74bab437931c0970ae83ea33ae5deb
jasminbee25 <jasminbagom1208@gmail.com>
2025-12-06 23:15:20 +0600

    Update home_screen.dart

--- Assignment 6/home_screen.dart
+++ Assignment 6/home_screen.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
+import 'profile_screen.dart';
+import 'info_screen.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProfileScreen()),
+                );
+              },
+              child: const Text("Go to Profile Page"),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const InfoScreen()),
+                );
+              },
+              child: const Text("Go to About Page"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
