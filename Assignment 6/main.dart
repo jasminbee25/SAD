6aaa6593387afe4b37a6f455a65ec1b640d412a3
jasminbee25 <jasminbagom1208@gmail.com>
2025-12-06 23:14:38 +0600

    Update main.dart

--- Assignment 6/main.dart
+++ Assignment 6/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/info_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
