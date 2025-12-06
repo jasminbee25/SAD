7cf2f1c2e3cad4049a602ed668bd75550879169f
jasminbee25 <jasminbagom1208@gmail.com>
2025-12-06 23:28:56 +0600

    Update score_page.dart

--- Assignment 6/score_page.dart
+++ Assignment 6/score_page.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class ScoreScreen extends StatelessWidget {
+  const ScoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Score Page"),
+        backgroundColor: Colors.orange,
+      ),
+      body: const Center(
+        child: Text(
+          "Your Score: 89/100",
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
