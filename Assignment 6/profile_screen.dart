b6db910708b46cf103eced821afeb83042c2adef
jasminbee25 <jasminbagom1208@gmail.com>
2025-12-06 23:16:20 +0600

    Update profile_screen.dart

--- Assignment 6/profile_screen.dart
+++ Assignment 6/profile_screen.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile Page"),
+        backgroundColor: Colors.green,
+      ),
+      body: const Center(
+        child: Text(
+          "This is the Profile Page.",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
