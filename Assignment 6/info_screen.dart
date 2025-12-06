d2fe3d818a79b4b205ee4b8b268d430d68a73e96
jasminbee25 <jasminbagom1208@gmail.com>
2025-12-06 23:27:57 +0600

    Update info_screen.dart

--- Assignment 6/info_screen.dart
+++ Assignment 6/info_screen.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class InfoScreen extends StatelessWidget {
+  const InfoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("About Page"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: const Center(
+        child: Text(
+          "This is the About Page.",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
