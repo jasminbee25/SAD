af9d53112a5b527741d12aef00dfd2d4a904b312
jasminbee25 <jasminbagom1208@gmail.com>
2025-12-06 23:25:24 +0600

    Update main.dart

--- Assignment 6/main.dart
+++ Assignment 6/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
-import 'screens/profile_screen.dart';
-import 'screens/info_screen.dart';
 
 void main() {
   runApp(const MyApp());
